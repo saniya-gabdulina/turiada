606f866b5649bd9b46dd3612753aeb6a3019e248
saniya.gabdulina <saniya.gabdulina@gmail.com>
2020-03-27 23:12:40 +0600

    C:/Program Files/Git/

--- js/main.js
+++ js/main.js
@@ -18,7 +18,25 @@ $(document).ready(function () {
 				}
 			},
 			{
-				breakpoint: 999,
+				breakpoint: 1030,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+					arrows: false,
+					infinite: true,
+					dots: true,
+				}
+			},
+			// {
+			// 	breakpoint: 999,
+			// 	settings: {
+			// 		slidesToShow: 2,
+			// 		slidesToScroll: 1,
+			// 	}
+			// },
+
+			{
+				breakpoint: 580,
 				settings: {
 					slidesToShow: 2,
 					slidesToScroll: 1,
@@ -27,9 +45,8 @@ $(document).ready(function () {
 					dots: true,
 				}
 			},
-
 			{
-				breakpoint: 580,
+				breakpoint: 480,
 				settings: {
 					slidesToShow: 1,
 					slidesToScroll: 1
@@ -58,7 +75,7 @@ $(document).ready(function () {
 				}
 			},
 			{
-				breakpoint: 1000,
+				breakpoint: 1030,
 				settings: {
 					slidesToShow: 3,
 					slidesToScroll: 1,
@@ -71,14 +88,20 @@ $(document).ready(function () {
 				breakpoint: 768,
 				settings: {
 					slidesToShow: 2,
-					slidesToScroll: 1
+					slidesToScroll: 1,
+					arrows: false,
+					infinite: true,
+					dots: true
 				}
 			},
 			{
-				breakpoint: 580,
+				breakpoint: 569,
 				settings: {
 					slidesToShow: 1,
-					slidesToScroll: 1
+					slidesToScroll: 1,
+					arrows: true,
+					infinite: true,
+					dots: false
 				}
 			},
 			{
@@ -98,9 +121,52 @@ $(document).ready(function () {
 $(window).resize(function() {
 	if ($('.mentors-card.slick-slider').length === 0  && $(window).width() <= 768) {
 		console.log('Slick create');
-		$('.mentors-card').slick();
+		$('.mentors-card').slick({
+			dots: false,
+			arrows: true,
+			infinite: true,
+			speed: 300,
+			slidesToScroll: 1,
+			slidesToShow: 3,
+			responsive: [{
+				breakpoint: 440,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+					infinite: true,
+					dots: true,
+				}
+			}]
+			
+			
+		});
 	} else if ($('.mentors-card.slick-slider').length !== 0 && $(window).width() > 768) {
 		console.log('Slick destroyed');
 		$('.mentors-card.slick-slider').slick('unslick');
+	}
+
+
+	if ($('.program-card.slick-slider').length === 0  && $(window).width() <= 768) {
+		console.log('Slick create');
+		$('.program-card').slick({
+			dots: false,
+			arrows: true,
+			infinite: true,
+			speed: 300,
+			slidesToScroll: 1,
+			slidesToShow: 3,
+			responsive: [{
+				breakpoint: 440,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					infinite: true,
+					dots: true
+				}
+			}]
+		});
+	} else if ($('.program-card.slick-slider').length !== 0 && $(window).width() > 768) {
+		console.log('Slick destroyed');
+		$('.program-card.slick-slider').slick('unslick');
 	}
 })
