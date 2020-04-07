8e08a484f4ae6ec9dfe45fd8f4338dd89b1d1000
saniya.gabdulina <saniya.gabdulina@gmail.com>
2020-04-07 15:59:58 +0600

    C:/Program Files/Git/

--- send.php
+++ send.php
@@ -1,6 +1,18 @@
 <?php
-
-//send email
-    mail("saniyakadyrbekova@gmail.com", "Name", 'asd');
-
-?>
+$method = $_S ERVER['REQUEST_METHOD'];
+//Script Foreach
+$c = true;
+$admin_email = 'saniyakadyrbekova@gmail.com';
+$userName = trim($_POST["userName"]);
+$userEmail  = trim($_POST["userEmail"]);
+$form_subject = 'Заявка с сайта Туриада';
+$message = "Новая заявка с сайта ".$userName.$userEmail;
+function adopt($text) {
+	return '=?UTF-8?B?'.base64_encode($text).'?=';
+}
+$headers = "MIME-Version: 1.0" . PHP_EOL .
+"Content-Type: text/html; charset=utf-8" . PHP_EOL .
+'From: '.adopt($project_name).' <'.$admin_email.'>' . PHP_EOL .
+'Reply-To: '.$admin_email.'' . PHP_EOL;
+// mail($admin_email, adopt($form_subject), $message, $headers );
+mail($admin_email, adopt($form_subject), $message, $headers );
